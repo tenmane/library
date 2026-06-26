f435b53094a7d2eccbb46d06885f5163ca54eb45
tenmane <235837458+tenmane@users.noreply.github.com>
2026-06-26 15:37:08 +0400

    feat: create function to display books Implement the displayBooks() function

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const myLibrary = [];
+const addButton = document.querySelector(".add-button");
 
-function Book(){
-
+Variables = {
+  libraryContainer: document.querySelector("#library-container"),
+}
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(){
-  
+function addBookToLibrary(name, author, pages) {
+  const book = new Book(name, author, pages);
+  myLibrary.push(book);
 }
+
+function displayBooks(books) {
+  for (book of books) {
+
+    let bookContainer = document.createElement("div");
+    let bookName = document.createElement("h2");
+    let bookAuthor = document.createElement("h3");
+    let bookPages = document.createElement("p");
+
+    bookName.textContent = book.name;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+
+    bookContainer.append(bookName);
+    bookContainer.append(bookAuthor);
+    bookContainer.append(bookPages);
+    Variables.libraryContainer.append(bookContainer);
+  }
+}
+displayBooks(myLibrary);
