15e6e54ad7696c5386a71b42d67c1048994244e2
tenmane <235837458+tenmane@users.noreply.github.com>
2026-06-27 21:34:46 +0400

    feat: add read status button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const addButton = document.querySelector(".add-button");
 const bookFormModal = document.querySelector("#book-adder");
 const submitFormButton = document.querySelector(".submit-book-form");
 const libraryContainer = document.querySelector("#library-container");
+const readButton = document.querySelector("#read");
 
 addButton.addEventListener("click", function () {
   bookFormModal.showModal();
@@ -14,21 +15,24 @@ submitFormButton.addEventListener("click", function (e) {
   const title = document.querySelector("#title");
   const author = document.querySelector("#author");
   const pages = document.querySelector("#pages");
-  addBookToLibrary(title.value, author.value, pages.value);
+  const radioStatus = readButton.checked;
+
+  addBookToLibrary(title.value, author.value, pages.value, radioStatus);
   displayBooks(myLibrary);
   bookFormModal.close();
   bookFormModal.querySelector("form").reset();
 })
 
-function Book(name, author, pages) {
+function Book(name, author, pages, status) {
   this.name = name;
   this.author = author;
   this.pages = pages;
   this.id = crypto.randomUUID();
+  this.status = status;
 }
 
-function addBookToLibrary(name, author, pages) {
-  const book = new Book(name, author, pages);
+function addBookToLibrary(name, author, pages, status) {
+  const book = new Book(name, author, pages, status);
   myLibrary.push(book);
 }
 
@@ -42,12 +46,33 @@ function displayBooks(books) {
     const bookName = document.createElement("h2");
     const bookAuthor = document.createElement("h3");
     const bookPages = document.createElement("p");
+    const readButton = document.createElement("button");
+    readButton.classList.add("book-status");
 
     bookName.textContent = book.name;
     bookAuthor.textContent = book.author;
     bookPages.textContent = book.pages;
 
-    bookContainer.append(bookName, bookAuthor, bookPages);
+    if (book.status) {
+      readButton.textContent = "Read";
+    }
+    else {
+      readButton.textContent = "Not Read";
+      readButton.classList.add("book-unread");
+    }
+    readButton.addEventListener("click", function () {
+      readButton.classList.toggle("book-unread");
+      if (readButton.textContent === "Read") {
+        readButton.textContent = "Not Read";
+        book.status = false;
+      }
+      else {
+        readButton.textContent = "Read";
+        book.status = true;
+      }
+    })
+
+    bookContainer.append(bookName, bookAuthor, bookPages, readButton);
     libraryContainer.append(bookContainer);
   }
 }
